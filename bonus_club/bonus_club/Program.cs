af4124cfc35d8e8b2fbd3a83c0aa5a6b1efb110c
Сорокин Андрей <a_sorokin@priminvestor.lan>
2020-11-16 16:31:05 +1000

    fst commit

---
+++ bonus_club/bonus_club/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonus_club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BK_DB_Methods.get_alarm_pds_pays().Rows.Count.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bonus_club/bonus_club/classes/BK_DB_Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace bonus_club
+{
+    class BK_DB_Methods
+    {
+        static string str = "uid=crm_usr;pwd=zTQrW4HTPA;Initial Catalog=Vlad.CRM;Data Source=192.168.100.5";
+
+
+        public static DataTable get_alarm_pds_pays()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter(
+                "	select CardId, count(*) from Orders where RkOrderDate > GETDATE() - 2 group by CardId having count(*) > 1	 "
+
+                , str);
+
+            DataTable DT = new DataTable();
+            sda.Fill(DT);
+            return DT;
+        }
+
+    }
+}
