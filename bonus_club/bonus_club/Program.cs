d6378c8dfd434cb950ff8200d802ed660046d8cf
Сорокин Андрей <a_sorokin@priminvestor.lan>
2020-11-18 16:16:56 +1000

    middle commit

--- bonus_club/bonus_club/Program.cs
+++ bonus_club/bonus_club/Program.cs
@@ -11,6 +11,9 @@ namespace bonus_club
     {
         static void Main(string[] args)
         {
+            //Grafana_DB_Methods.ins_user_data("111", "aaaaa", "bbbb", "", new DateTime(2000, 11, 1));
+
+            //Console.ReadLine();
             foreach (DataRow row in BK_DB_Methods.get_alarm_pds_users().Rows)
             {
                // если пользователь westrest нету в графане
@@ -18,6 +21,32 @@ namespace bonus_club
                 {
                     //добавляем
                     Console.WriteLine("no");
+
+                    Grafana_DB_Methods.ins_user_data(
+                        row["CardId"].ToString()
+                        , row["FirstName"].ToString()
+                        , row["LastName"].ToString()
+                        , ""
+                        ,
+                        new DateTime(
+                          int.Parse(
+                        row["Birthday"].ToString()[6].ToString() +
+                        row["Birthday"].ToString()[7].ToString() +
+                        row["Birthday"].ToString()[8].ToString() +
+                        row["Birthday"].ToString()[9].ToString()
+                        )
+                        , int.Parse(
+                        row["Birthday"].ToString()[3].ToString() +
+                        row["Birthday"].ToString()[4].ToString()
+                        )
+                        , int.Parse(
+                        row["Birthday"].ToString()[0].ToString() +
+                        row["Birthday"].ToString()[1].ToString()
+                        ))
+
+                        );
+
+                   // cl.FirstName, cl.LastName, cl.Birthday, o.CardId
                 } else {
                     
                     Console.WriteLine("yes");
--- bonus_club/bonus_club/classes/Grafana_DB_Methods.cs
+++ bonus_club/bonus_club/classes/Grafana_DB_Methods.cs
@@ -28,5 +28,52 @@ namespace bonus_club
             }
 
         }
+
+
+        public static void ins_user_data(string card_id, string fst_name, string scnd_name, string tel,  DateTime birthday)
+        {
+            using (SqlConnection connection = new SqlConnection(str))
+            {
+                //параметр определения строки которую правим
+                SqlCommand sp = new SqlCommand(
+                   "	INSERT INTO [dbo].[westrest_clients]	 " +
+                    "	           ([id]	 " +
+                    "	           ,[fst_name]	 " +
+                    "	           ,[scnd_name]	 " +
+                    "	           ,[tel]	 " +
+                    "	           ,[birthday])	 " +
+                    "	     VALUES	 " +
+                    "	           (@card_id	 " +
+                    "	           ,@fst_name	 " +
+                    "	           ,@scnd_name	 " +
+                    "	           ,@tel     " +
+                    "	           ,@birthday)	 "
+            , connection);
+                sp.Connection.Open();
+
+                SqlParameter spar_card_id = sp.Parameters.Add("@card_id", SqlDbType.Int);
+                spar_card_id.Direction = ParameterDirection.Input;
+                spar_card_id.Value = card_id;
+
+                SqlParameter spar_fst_name = sp.Parameters.Add("@fst_name", SqlDbType.VarChar, 200);
+                spar_fst_name.Direction = ParameterDirection.Input;
+                spar_fst_name.Value = fst_name;
+
+                SqlParameter spar_scnd_name = sp.Parameters.Add("@scnd_name", SqlDbType.VarChar, 200);
+                spar_scnd_name.Direction = ParameterDirection.Input;
+                spar_scnd_name.Value = scnd_name;
+
+                SqlParameter spar_tel = sp.Parameters.Add("@tel", SqlDbType.VarChar, 100);
+                spar_tel.Direction = ParameterDirection.Input;
+                spar_tel.Value = tel;
+
+                SqlParameter spar_birthday = sp.Parameters.Add("@birthday", SqlDbType.DateTime);
+                spar_birthday.Direction = ParameterDirection.Input;
+                spar_birthday.Value = birthday;
+
+                sp.ExecuteNonQuery();
+
+            }
+        }
     }
 }
